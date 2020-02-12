3f7e357d812ab091119c6233258c81995e63cb16
Devin Hicks <devinhicks.az@gmail.com>
2020-02-12 09:21:14 -0700

    broken!

---
+++ Steering_Behaviors/Assets/Scripts/Avoiding.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avoiding : Kinematic
+{
+    CollisionAvoidance moveType;
+    LookWhereYoureGoing rotateType;
+
+    public Kinematic[] avoidingTargets;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        moveType = new CollisionAvoidance();
+        moveType.character = this;
+        moveType.targets = avoidingTargets;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        steeringUpdate = new SteeringOutput();
+        steeringUpdate.linear = moveType.getSteering().linear;
+        steeringUpdate.angular = moveType.getSteering().angular;
+        base.Update();
+    }
+}
---
+++ Steering_Behaviors/Assets/Scripts/CollisionAvoidance.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAvoidance
+{
+    public Kinematic character;
+    public Kinematic[] targets;
+
+    public float maxAcceleration = 3f;
+    public float radius = 1f;
+
+    public SteeringOutput getSteering()
+    {
+        /// step 1: see if there's impending danger
+        
+        // find target that's closest to collision
+        // store the first collision time
+        float shortestTime = float.PositiveInfinity;
+
+        // store our data
+        Vector3 relativePos;
+
+        // store that target and it's data
+        Kinematic firstTarget = null;
+        float firstMinSeparation;
+        float firstDistance;
+        Vector3 firstRelativePos;
+        Vector3 firstRelativeVel;
+
+        // loop through targets
+        foreach (Kinematic target in targets)
+        {
+            // get time until collision
+            Debug.Log("for looping");
+            relativePos = target.transform.position - character.transform.position;
+            Vector3 relativeVel = character.linearVelocity - target.linearVelocity;
+            float relativeSpeed = relativeVel.magnitude;
+            float timeToCollision = Vector3.Dot(relativePos, relativeVel) /
+                (relativeSpeed * relativeSpeed);
+
+            // check if we'll actually collide
+            float distance = relativePos.magnitude;
+            float minSeparation = distance - relativeSpeed * timeToCollision;
+            if (minSeparation > 2 * radius)
+            {
+                continue;
+            }
+
+            // check if it is the shortest
+            if (timeToCollision > 0 && timeToCollision < shortestTime)
+            {
+                // store the time, target and other data
+                shortestTime = timeToCollision;
+                firstTarget = target;
+                firstMinSeparation = minSeparation;
+                firstDistance = distance;
+                firstRelativePos = relativePos;
+                firstRelativeVel = relativeVel;
+            }
+        }
+
+        /// step 2: do somethin about it
+
+        // if no target, exit
+        if (firstTarget == null)
+        {
+            Debug.Log("SAFE!");
+            return null;
+        }
+
+        /// if we're going to hit exactly, or if we're already colliding
+        /// do the steering based on current Pos
+
+        Debug.Log("DANGER - gonna hit " + firstTarget);
+        //return null;
+
+        SteeringOutput result = new SteeringOutput();
+
+        float dotResult = Vector3.Dot(character.linearVelocity.normalized,
+            firstTarget.linearVelocity.normalized);
+        if (dotResult < -0.9)
+        {
+            result.linear = -firstTarget.transform.right;
+        }
+        else
+        {
+            result.linear = -firstTarget.linearVelocity;
+        }
+        result.linear.Normalize();
+        result.linear *= maxAcceleration;
+        result.angular = 0;
+        return result;
+    }
+}
